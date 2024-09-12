d8eda745b4f5b21b1fdbd091f67dddc4f640cde1
Jianjin Xu <xujj15@gmail.com>
2024-09-12 09:34:56 -0400

    save

--- scripts/infer.sh
+++ scripts/infer.sh
@@ -1,9 +1,10 @@
 # Example usage
 EXPORT_VIDEO=true
 EXPORT_MESH=true
-INFER_CONFIG="./configs/infer-b.yaml"
-#MODEL_NAME="zxhezexin/openlrm-mix-large-1.1"
-MODEL_NAME="expr/releases/lrm-objaverse/small-dummyrun/step_004680"
+INFER_CONFIG="./configs/infer-l.yaml"
+MODEL_NAME="zxhezexin/openlrm-mix-large-1.1"
+#MODEL_NAME="expr/releases/lrm-objaverse/small-dummyrun/step_004680"
+#MODEL_NAME="expr/releases/lrm-objaverse/large/step_007000"
 IMAGE_INPUT="/home/jianjinx/data2/HHRGaussian-priv/data/PanoHeadDense/064061/images/IMG_0000.jpg"
 
 python -m openlrm.launch infer.lrm --infer $INFER_CONFIG model_name=$MODEL_NAME image_input=$IMAGE_INPUT export_video=$EXPORT_VIDEO export_mesh=$EXPORT_MESH
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,5 @@
 ACC_CONFIG="./configs/accelerate-train.yaml"
 TRAIN_CONFIG="./configs/train-panohead-large.yaml"
+LOG_LEVEL="INFO"
 
 accelerate launch --config_file $ACC_CONFIG -m openlrm.launch train.lrm --config $TRAIN_CONFIG
